78eac6c07702102b8a20a499b06d20e8dd75f738
Morgan 🏴󠁧󠁢󠁥󠁮󠁧󠁿 <107074986+N4GR@users.noreply.github.com>
2024-07-22 05:20:58 +0100

    Added colouring to terminal logs

--- log.py
+++ log.py
@@ -1,5 +1,12 @@
 from datetime import datetime
 
+MAGENTA = '\033[35m'
+BRIGHT_RED = '\033[91m'
+BRIGHT_GREEN = '\033[92m'
+BRIGHT_BLUE = '\033[94m'
+
+RESET = '\033[0m'
+
 class logging():
     def __init__(self) -> None:
         now = datetime.now()
@@ -7,7 +14,10 @@ class logging():
         self.time = now.strftime("%d/%m/%Y %H:%M:%S")
     
     def note(self, message: str) -> str:
-        return f"{self.time} | NOTE  | {message}"
+        return f"{MAGENTA}{self.time}{RESET} |  {BRIGHT_BLUE}STATE{RESET}  | {message}"
 
+    def success(self, message: str) -> str:
+        return f"{MAGENTA}{self.time}{RESET} | {BRIGHT_GREEN}SUCCESS{RESET} | {message}"
+    
     def error(self, message: str) -> str:
-        return f"{self.time} | ERROR | {message}"
+        return f"{MAGENTA}{self.time}{RESET} |  {BRIGHT_RED}ERROR{RESET}  | {message}"
