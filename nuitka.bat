69e33139415ae89978268d7c0c63c3717e913d50
lobotomyx <lbatvt@gmail.com>
2024-07-28 07:38:41 -0400

    Create nuitka.bat

---
+++ nuitka.bat
@@ -0,0 +1,1 @@
+nuitka --standalone --onefile --main=main.py --follow-imports --assume-yes-for-downloads --output-filename="BoxArtGen.exe" --enable-plugin=tk-inter --windows-icon-from-ico="assets\\logo.ico"
